2355fba26ded461c4793782aea498354fa531606
Aadit Yadav <yaaditya191@gmail.com>
2025-06-30 17:45:47 +0545

    Updated Not found page

--- src/components/NavbarSearch/SearchInput.jsx
+++ src/components/NavbarSearch/SearchInput.jsx
@@ -1,27 +1,25 @@
-import React, { useContext } from 'react'
-import { StoreContext } from '../../context/StoreContext'
-import { assets } from '../../assets/assets';
+import React, { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
+import { assets } from "../../assets/assets";
 
 const SearchInput = () => {
-   
-    const {searhItem , handleSearch} = useContext(StoreContext);
+  const { searhItem, handleSearch } = useContext(StoreContext);
 
   return (
     <>
-     
-     <div className='navbar-search' >
-        <img src={assets.search_icon} alt="search"
-        className='search-icon' />
+      <div className="navbar-search">
+        <img src={assets.search_icon} alt="search" className="search-icon" />
 
-        <input type="text"
-        className='navbar-searchbar'
-        placeholder='Enter food Item'
-        value={searhItem} 
-        onChange={(e) => handleSearch(e.target.value) }  />
-     </div>
-    
+        <input
+          type="text"
+          className="navbar-searchbar"
+          placeholder="Search food Item..."
+          value={searhItem}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Make sure React Router is set up
+import notFoundImage from "../assets/NOTFOUND.svg";
+
 const NotFound = () => {
   return (
     <div style={{ padding: "2rem", textAlign: "center" }}>
+      <img
+        src={notFoundImage}
+        alt="Not Found"
+        style={{ width: "500px", height: "auto" }}
+      />
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
+      
+      <Link to="/" style={{
+        display: "inline-block",
+        marginTop: "1.5rem",
+        padding: "0.75rem 1.5rem",
+        backgroundColor: "#fa7939",
+        color: "#fff",
+        textDecoration: "none",
+        borderRadius: "4px",
+        fontSize: "1rem"
+      }}>
+        Go to Home
+      </Link>
     </div>
   );
 };
